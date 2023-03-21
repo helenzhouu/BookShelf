3c035a22cc53cb3d23574f6ee63e162a42678946
hzhou2 <hzhou2@students.cs.ubc.ca>
2023-03-20 21:32:28 -0700

    start of phase 3

---
+++ src/main/ui/BookShelfUI.java
@@ -0,0 +1,17 @@
+package ui;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+// represents BookApp's main frame
+public class BookShelfUI extends JFrame {
+
+    public BookShelfUI() {
+        JFrame bookFrame = new JFrame();
+        JPanel bookPanel = new JPanel();
+        bookPanel.setLayout(new BorderLayout());
+
+
+    }
+}
